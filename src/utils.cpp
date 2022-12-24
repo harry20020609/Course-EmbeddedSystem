2f04805349082796642bc995b61c406edc516d72
Seedoil <1072825625@qq.com>
2022-12-24 14:17:01 +0800

    submit

--- src/utils.cpp
+++
@@ -1,9 +0,0 @@
-#include <lab2/utils.hpp>
-
-namespace lab2_utils {
-
-double vecLength(const geometry_msgs::Vector3 &v) {
-  return std::sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-}  // namespace lab2_utils
